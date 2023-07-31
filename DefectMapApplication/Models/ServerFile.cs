fd9053a7761ea8ec997bd232001fcdbb271d1c5e
TeaFaris <teafaris@organizatsiya.site>
2023-07-31 15:03:49 +0500

    Added server models

---
+++ DefectMapApplication/Models/ServerFile.cs
@@ -0,0 +1,10 @@
+﻿namespace DefectMapApplication.Models
+{
+    public class ServerFile
+    {
+        public Guid Id { get; init; }
+        public string FileName { get; init; }
+        public string StoredFileName { get; init; }
+        public string ContentType { get; init; }
+    }
+}
